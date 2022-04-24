3bafa986a1bc3966d7c66d9ee57144dce9484d92
Антон Жданов <sp.play2005@gmail.com>
2022-04-24 12:22:43 +0500

    25 04 updated

--- data base.py
+++ data base.py
@@ -5,53 +5,57 @@ from time import sleep
 all_memes = []
 
 
-def create_db():
+# Заполняем таблицу
+def fill_db(base, sql):
     global all_memes
+    all_memes = get_album_memes(owner_id=-197700721, album_id=281940823)
+    all_memes.extend(get_wall_memes(owner_id=-150550417, posts_amount=100))
+    sleep(0.5)
+
+    for item in all_memes:
+        if len(item) > 13:
+            try:
+                if len(item['text']) > 100 or item['marked_as_ads'] == 1 or not len(item['attachments']) == 1 or not \
+                        item['attachments'][0]['type'] == 'photo':
+                    continue
+            except Exception:
+                print(item)
+
+            picture_link = find_best_picture(item['attachments'][0]['photo']['sizes'])
+            item_type = "post"
+            user_name = get_group_name_by_id(abs(item['owner_id']))
+        else:
+            picture_link = find_best_picture(item['sizes'])
+            item_type = "photo"
+            user_name = get_user_name_by_id(item['user_id'])
+        owner_id = item['owner_id']
+        item_id = item['id']
+        likes = item['likes']['count']
+
+        sql.execute("INSERT INTO vezdecod VALUES(?,?,?,?,?,?);",
+                    (owner_id, user_name, item_id, item_type, likes, picture_link))
+        base.commit()
+        sleep(0.15)
+
+
+if __name__ == '__main__':
     # Создаем базу данных
     db = sqlite3.connect("vezdecod.db")
     cursor = db.cursor()
     cursor.execute("""CREATE TABLE IF NOT EXISTS vezdecod(
-        owner_id INTEGER,
-        user_name TEXT,
-        item_id INTEGER,
-        item_type TEXT,
-        likes INTEGER,
-        picture_link TEXT);
-    """)
+            owner_id INTEGER,
+            user_name TEXT,
+            item_id INTEGER primary key,
+            item_type TEXT,
+            likes INTEGER,
+            picture_link TEXT);
+        """)
     db.commit()
-
-    all_memes = get_vezdecod_memes()
-    all_memes.extend(get_reddit_memes())
-    sleep(0.5)
-
-    # Заполняем таблицу
     try:
-        for item in all_memes:
-            if len(item) > 13:
-                try:
-                    if len(item['text']) > 100 or item['marked_as_ads'] == 1 or not len(item['attachments']) == 1 or not item['attachments'][0]['type'] == 'photo':
-                        continue
-                except Exception:
-                    print(item)
+        fill_db(db, cursor)
 
-                picture_link = find_best_picture(item['attachments'][0]['photo']['sizes'])
-                item_type = "post"
-                user_name = get_group_name(abs(item['owner_id']))
-            else:
-                picture_link = find_best_picture(item['sizes'])
-                item_type = "photo"
-                user_name = get_user_name(item['user_id'])
-            owner_id = item['owner_id']
-            item_id = item['id']
-            likes = item['likes']['count']
-
-            cursor.execute("INSERT INTO vezdecod VALUES(?,?,?,?,?,?);", (owner_id, user_name, item_id, item_type, likes, picture_link))
-            db.commit()
-            sleep(0.15)
+    except Exception as e:
+        print(e)
     finally:
         db.close()
-
-
-if __name__ == '__main__':
-    create_db()
-    print(0)
+    print('Task have been finished!')
--- search_memes.py
+++ search_memes.py
@@ -4,10 +4,12 @@ from config import token
 API_VERSION = 5.131
 
 
-# Получаем мемы из альбома Вездекод
-def get_vezdecod_memes():
-    album_id = '281940823'
-    owner_id = '-197700721'
+# Возвращает мемы из указанного альбома в формате json
+# owner_id - id сообщества (записывается с минусом вначале '-124631533')
+# album_id - id альбома в сообществе
+def get_album_memes(owner_id: int, album_id: int):
+    # album_id = '281940823'
+    # owner_id = '-197700721'
     response = requests.get('https://api.vk.com/method/photos.get',
                             params={
                                 'access_token': token,
@@ -19,26 +21,34 @@ def get_vezdecod_memes():
     return response.json()['response']['items']
 
 
-# Получаем мемы со стены сообщества Reddit
-def get_reddit_memes():
-    owner_id = '-150550417'
+# Возвращает мемы со стены указанного сообщества в формате json
+# owner_id - id сообщества (записывается С МИНУСОМ вначале '-124631533')
+# posts_amount - кол-во взятых постов
+def get_wall_memes(owner_id: int, posts_amount: int):
+    # owner_id = -150550417
+    count = 100 if posts_amount >= 100 else posts_amount
+    watched_posts = 0
     offset = 0
     all_posts = []
-    while offset < 200:
+
+    while posts_amount > watched_posts:
         response = requests.get('https://api.vk.com/method/wall.get',
                                 params={
                                     'access_token': token,
                                     'v': API_VERSION,
                                     'owner_id': owner_id,
-                                    'count': 100,
+                                    'count': count,
                                     'offset': offset
                                 })
         all_posts.extend(response.json()['response']['items'])
         offset += 100
+        watched_posts += count
+        count = posts_amount - watched_posts
     return all_posts
 
 
-# Находит картинку лучшего качества
+# Находит картинку лучшего качества среди предложенных
+# Получает на вход json путь к фотографиям до 'sizes' (item['attachments'][0]['photo']['sizes'])
 def find_best_picture(items):
     best_link = " "
     resolution = -1
@@ -50,7 +60,7 @@ def find_best_picture(items):
 
 
 # Находит имя пользователя по id
-def get_user_name(user_id):
+def get_user_name_by_id(user_id):
     response = requests.get('https://api.vk.com/method/users.get',
                             params={
                                 'access_token': token,
@@ -64,7 +74,8 @@ def get_user_name(user_id):
 
 
 # Находит имя группы по id
-def get_group_name(owner_id):
+# owner_id - id сообщества (записывается БЕЗ минуса вначале '124631533')
+def get_group_name_by_id(owner_id):
     response = requests.get('https://api.vk.com/method/groups.getById',
                             params={
                                 'access_token': token,
